334ade04d1141f014296eca081b9f2a920ac8964
NikolaPerleta39 <kovalski1515@gmail.com>
2019-12-11 22:09:29 +0100

    projekat

---
+++ nav.js
@@ -0,0 +1,33 @@
+document.write(`
+<nav class="navbar navbar-expand-md bg-white navbar-dark">
+        <div class="container-fluid">
+            <div class=container-fluid-image>     
+            <a class="navbar-link" href="index.html"><img class="logo-img" src="logo.jpg"></a>
+            </div>
+
+            <button class="navbar-toggler bg-dark" type="button" data-toggle="collapse"
+            data-target="#navbarResponsive">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.html">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="about.html">About Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="destinations.html">Destinations</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="galerija.html">Gallery</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="kontakt.html">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+`)
